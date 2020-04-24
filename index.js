e741851cdcaf79739ddc5a19b102c5043583d785
EnochTerryMize <enochterrymize@outlook.com>
2020-04-24 20:19:23 +0200

    First Commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const http = require("http");
+const cors = require("cors");
+const express = require("express");
+const socketio = require("socket.io");
+const { addUser, removeUser, getUser, getUsersInRoom } = require("./users.js");
+const PORT = process.env.PORT || 5000;
+const router = require("./router");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.use(router);
+app.use(cors());
+
+io.on("connection", (socket) => {
+  console.log("We have connection");
+  socket.on("join", ({ name, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, name, room });
+
+    if (error) return callback(error);
+
+    socket.emit("message", {
+      user: "admin",
+      text: `${(user, name)}, welcome to the room ${user.room}`,
+    });
+    socket.broadcast
+      .to(user.room)
+      .emit("message", { user: "admin", text: `${user.name}, has joined!` });
+    socket.join(user.room);
+    callback();
+  });
+
+  socket.on("sendMessage", () => {
+    const user = getUser(socket.id);
+
+    io.to(user.room).emit("message", { user: user.name, text: message });
+    callback();
+  });
+  socket.on("disconnect", () => {
+    console.log("User had left!!!");
+  });
+});
+
+server.listen(PORT, () => console.log(`Server has started on ${PORT}`));
